d7fbdca7f2cb638da9ec8d1a4a50a9f69804b8ab
SamTredgett <sam.tredgett@gmail.com>
2022-06-02 15:01:00 +0100

    Project file setup

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+// Gonna be writing small to-do app in rust/yew framework
+// messing around with this for future projects maybe for work
+// turns out you can interop between rust and Blazor webapps for the dotnet ecosystem
+
+
+// god knows how this will go...
+fn main() {
+    println!("Hello, world!");
+}
